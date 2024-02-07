8ecd5ae4b7d8dbed6126099e68ffadc63b091b7f
Vasudeva8278 <vasudevsanchapu@gmail.com>
2024-02-07 15:44:37 +0530

    commit

--- src/Pages/Service.js
+++ src/Pages/Service.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Service = () => {
   return (
-    <>
+    <><div className='about-content'>
+    <br></br><br></br>
+ 
+  </div>
+
     <div>
       Service <h1>Vasu Deva Rao (Vasi)</h1>
     </div>
