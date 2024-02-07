7d260f027a4850e1e4dd8164a6fab0132c765818
Pavani-Developer <pavani65108@gmail.com>
2024-02-07 10:41:06 +0530

    commit

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,4 +1,3 @@
-import { Block } from '@mui/icons-material';
 import React from 'react';
 import write_us_image from '../Assets/feedback-icon.png';
 import location_image from '../Assets/maps-pin-line-icon.png';
@@ -16,21 +15,21 @@ const Contact = () => {
                                     width:'170px',borderRadius:'50%', padding:'5px'}}>
               <div style={{border:'5px solid #a0ce4e',height:'150px',
                                     width:'150px',borderRadius:'50%', padding:'20px',backgroundColor:'#a0ce4e'}}>
-                <img style={{height:'100px'}} src={phone_outline_icon} alt='No image'/>
+                <img style={{height:'100px'}} src={phone_outline_icon} />
               </div>
           </div>
           <div style={{border:'5px solid #a0ce4e',height:'170px',
                                     width:'170px',borderRadius:'50%', padding:'5px'}}>
               <div style={{border:'5px solid #a0ce4e',height:'150px',
                                     width:'150px',borderRadius:'50%', padding:'20px',backgroundColor:'#a0ce4e'}}>
-                <img style={{height:'100px'}} src={write_us_image} alt='No image'/>
+                <img style={{height:'100px'}} src={write_us_image} />
               </div>
           </div>
           <div style={{border:'5px solid #a0ce4e',height:'170px',
                                     width:'170px',borderRadius:'50%', padding:'5px'}}>
               <div style={{border:'5px solid #a0ce4e',height:'150px',
                                     width:'150px',borderRadius:'50%', padding:'25px 0px 0px 30px',backgroundColor:'#a0ce4e'}}>
-                <img style={{height:'100px'}} src={location_image} alt='No image'/>
+                <img style={{height:'100px'}} src={location_image}/>
               </div>
           </div>
         </div>
